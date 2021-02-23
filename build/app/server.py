96b53763f8044260656baff6a913dc740ab73682
Petrov Ivan <petrov_ivan@forecsys.ru>
2021-02-23 22:49:52 +0300

    initial commit

--- build/app/server.py
+++ build/app/server.py
@@ -1,8 +1,28 @@
 import uvicorn
+import yaml
 
 
 async def app(scope, receive, send):
     ...
 
+
+def get_default_port():
+    return 5000
+
+
+try:
+    with open('settings.yml') as f:
+        settings = yaml.safe_load(f)
+    app_port = settings['server']['container-port']
+    if type(app_port) != int:
+        app_port = get_default_port()
+        print("Settings file is corrupted: port value is not a number. Using default port: " + str(app_port))
+except FileNotFoundError:
+    app_port = get_default_port()
+    print("File not found. Using default port: " + str(app_port))
+except KeyError:
+    app_port = get_default_port()
+    print("Settings file is corrupted: port key not found. Using default port: " + str(app_port))
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=app_port, log_level="info")
