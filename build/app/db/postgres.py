d7abf1af405241c069a9b01d35f8d03dccf4b96c
Petrov Ivan <petrov_ivan@forecsys.ru>
2021-02-27 01:54:45 +0300

    db + typo fix

--- build/app/db/postgres.py
+++ build/app/db/postgres.py
@@ -12,7 +12,15 @@ def connect():
     db_user = safe.get_value('DB_USER')
     db_password = safe.get_value('DB_PASSWORD')
     db_local_port = safe.get_value('DB_LOCAL_PORT')
-
-    conn = psycopg2.connect(dbname=db_app, user=db_user,
-                            password=db_password, host="localhost", port=db_local_port)
-    cursor = conn.cursor()
+    try:
+        conn = psycopg2.connect(dbname=db_app, user=db_user,
+                                password=db_password, host="localhost", port=db_local_port)
+        cursor = conn.cursor()
+        logging.info("Connected to the database!")
+        return cursor
+    except psycopg2.OperationalError:
+        logging.error("Failed connecting to the database on localhost:"+db_local_port)
+        return None
+    except IndentationError:
+        logging.error("Failed to connect with current credentials")
+        return None
--- build/app/server.py
+++ build/app/server.py
@@ -21,8 +21,8 @@ app_port = safe.get_value('CONTAINER_PORT')
 logging.debug("Trying to get env info: " + str(app_port))
 if app_port is None:
     app_port = get_default_port()
-    logging.error("Env getting failed. Using default port: " + str(app_port))
+    logging.warning("Env getting failed. Using default port: " + str(app_port))
 
 if __name__ == "__main__":
-    postgres.connect()
+    cursor = postgres.connect()
     uvicorn.run("main:app", host="0.0.0.0", port=int(app_port), log_level="info")
