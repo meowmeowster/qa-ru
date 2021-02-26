61ff3c7b272db3088234c344e1f07cecb103eb20
Petrov Ivan <petrov_ivan@forecsys.ru>
2021-02-27 01:42:01 +0300

    db + typo fix

--- build/app/db/postgres.py
+++ build/app/db/postgres.py
@@ -2,8 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import psycopg2
+from auth import safe
+import logging
+import datetime
 
 
-conn = psycopg2.connect(dbname='database', user='db_user',
-                        password='mypassword', host='localhost')
-cursor = conn.cursor()
+def connect():
+    db_app = safe.get_value('DB_APP')
+    db_user = safe.get_value('DB_USER')
+    db_password = safe.get_value('DB_PASSWORD')
+    db_local_port = safe.get_value('DB_LOCAL_PORT')
+
+    conn = psycopg2.connect(dbname=db_app, user=db_user,
+                            password=db_password, host="localhost", port=db_local_port)
+    cursor = conn.cursor()
--- build/app/server.py
+++ build/app/server.py
@@ -3,6 +3,7 @@
 
 import uvicorn
 from auth import safe
+from db import postgres
 import logging
 import datetime
 
@@ -23,4 +24,5 @@ if app_port is None:
     logging.error("Env getting failed. Using default port: " + str(app_port))
 
 if __name__ == "__main__":
+    postgres.connect()
     uvicorn.run("main:app", host="0.0.0.0", port=int(app_port), log_level="info")
